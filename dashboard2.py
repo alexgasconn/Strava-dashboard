6f6a0af77ae244e14601892fde6e64608f2a23f1
Alex <alex.gascon1999@gmail.com>
2025-03-27 22:30:47 +0100

    imports removed for deploy

--- dashboard2.py
+++ dashboard2.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-import plotly.express as px
-import matplotlib.pyplot as plt
-import seaborn as sns
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
