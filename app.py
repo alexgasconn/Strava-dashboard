27cebcafe20206450ce633ade4f10cf92b45dd19
Alex Gascón <alex.gascon1999@gmail.com>
2025-06-04 08:29:49 +0200

    date filter n

--- app.py
+++ app.py
@@ -19,19 +19,19 @@ with st.sidebar:
     df = preprocess_data(df)
 
     # Date filter
-    if 'start_date' in df.columns:
-        min_date = pd.to_datetime(df['start_date']).min().date()
-        max_date = pd.to_datetime(df['start_date']).max().date()
-        start, end = st.date_input(
-            "Filter by date range",
-            [min_date, max_date],
-            min_value=min_date,
-            max_value=max_date
-        )
-        if isinstance(start, pd.Timestamp): start = start.date()
-        if isinstance(end, pd.Timestamp): end = end.date()
-        mask = (pd.to_datetime(df['start_date']).dt.date >= start) & (pd.to_datetime(df['start_date']).dt.date <= end)
-        df = df.loc[mask]
+    # if 'start_date' in df.columns:
+    min_date = pd.to_datetime(df['start_date']).min().date()
+    max_date = pd.to_datetime(df['start_date']).max().date()
+    start, end = st.date_input(
+        "Filter by date range",
+        [min_date, max_date],
+        min_value=min_date,
+        max_value=max_date
+    )
+    if isinstance(start, pd.Timestamp): start = start.date()
+    if isinstance(end, pd.Timestamp): end = end.date()
+    mask = (pd.to_datetime(df['start_date']).dt.date >= start) & (pd.to_datetime(df['start_date']).dt.date <= end)
+    df = df.loc[mask]
 
 
 # Tab navigation
